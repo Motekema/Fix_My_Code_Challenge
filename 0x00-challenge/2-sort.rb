2af735e008138ed3558ed104c2896c390d6b27ac
Motekema <motekema@gmail.com>
2023-11-24 06:01:18 +0000

    My commit

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,35 @@
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
+
+result = []
+
+ARGV.each do |arg|
+    # Skip if not an integer
+    next unless arg =~ /^-?\d+$/
+
+    # Convert to integer
+    i_arg = arg.to_i
+
+    # Insert the result at the right position
+    is_inserted = false
+    i = 0
+    l = result.size
+
+    while !is_inserted && i < l do
+        if result[i] <= i_arg
+            i += 1
+        else
+            result.insert(i, i_arg)
+            is_inserted = true
+            break
+        end
+    end
+
+    result << i_arg unless is_inserted
+end
+
+puts result
+
